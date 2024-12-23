3614a0f79b9c8194e76a4fd782aff921ada7a3d1
ch <mkmsacc@outlook.com>
2024-12-23 14:42:24 +0700

    Refactor: Move `commands` property retrieval outside task
    Moved the retrieval of the `commands` gradle property outside the task definition to improve code clarity and avoid redundant lookups. This change ensures better separation of concerns and simplifies debugging.

--- command/build.gradle.kts
+++ command/build.gradle.kts
@@ -2,22 +2,20 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 val commands = project.providers.gradleProperty("commands").get()
+
 tasks.register("sh") {
     description = "Executes a shell command"
     group = "Custom"
 
     doLast {
         try {
-            // Create and start the process
-            val process = ProcessBuilder(*commands.split(" ").toTypedArray())
-                .redirectErrorStream(true) // Redirects error stream to output stream
+            val process = ProcessBuilder("sh", "-c", "\"$commands\"")
+                .redirectErrorStream(true)
                 .start()
 
-            // Capture output
             val reader = BufferedReader(InputStreamReader(process.inputStream))
             reader.useLines { lines -> lines.forEach { println(it) } }
 
-            // Wait for the process to terminate and get exit code
             val exitCode = process.waitFor()
             println("Exit code: $exitCode")
         } catch (e: Exception) {
