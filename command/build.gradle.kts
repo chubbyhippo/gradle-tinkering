405681f61b959c8bd06b60b1341528ce799289bc
ch <mkmsacc@outlook.com>
2024-12-23 13:31:50 +0700

    Refactor "sh" task to use ProcessBuilder for execution
    Replaced the previous shell execution method with ProcessBuilder to handle commands more robustly. This change improves error handling by capturing output and exit codes and redirects error streams for clearer debugging. An exception handling mechanism is added to avoid runtime crashes.

--- command/build.gradle.kts
+++ command/build.gradle.kts
@@ -1,9 +1,27 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 tasks.register("sh") {
     description = "Executes a shell command"
     group = "Custom"
-    val commands = project.findProperty("commands") as String?
-    exec {
-        commandLine("sh", "-c", "\"$commands\"")
-        standardOutput = System.out
+
+    doLast {
+        val commands = project.findProperty("commands") as String?
+        try {
+            // Create and start the process
+            val process = ProcessBuilder(*commands!!.split(" ").toTypedArray())
+                .redirectErrorStream(true) // Redirects error stream to output stream
+                .start()
+
+            // Capture output
+            val reader = BufferedReader(InputStreamReader(process.inputStream))
+            reader.useLines { lines -> lines.forEach { println(it) } }
+
+            // Wait for the process to terminate and get exit code
+            val exitCode = process.waitFor()
+            println("Exit code: $exitCode")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
